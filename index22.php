eda9a05a223c7f30ea9f4ce4994941228ee1a3e4
Amanda Caven <amanda.caven@gmail.com>
2015-11-30 13:36:29 -0600

    added all files

---
+++ index22.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Amanda Caven's Graphic and Web Design Portfolio">
+    <meta name="author" content="Amanda Caven">
+    <meta name="keywords" content="Amanda Caven, amanda caven, graphic design, web design, graphic and web design, graphic designer, web designer, graphic designer hutchinson mn, web designer hutchinson mn">
+      
+    <title>Amanda Caven | Portfolio</title>
+    
+      <link rel="icon" href="_images/favicon.ico">
+      <link rel="stylesheet" href="css/animate.css">
+      
+<!------------LINKS & SCRIPTS-------->      
+          <!-- Bootstrap core CSS -->
+    <link href="bootstrap-3.3.5/dist/css/bootstrap.css" rel="stylesheet">
+
+    <link href="navbar-fixed-top.css" rel="stylesheet">
+
+    <script src="bootstrap-3.3.5/docs/assets/js/ie-emulation-modes-warning.js"></script>
+ <!---MY CSS STYLESHEET------->           
+    <link href="style.css" rel="stylesheet">
+      <link href="fonts/font-awesome-4.5.0/css/font-awesome.css" rel="stylesheet" type="text/css">
+      
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+
+      
+<!--------- SMOOTH SCROLL ------------->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+</script>
+<!--------- End of SMOOTH SCROLL ------------>
+
+<!------GALLERY SCRIPTS--------->
+
+<script type="text/javascript" src="js/jquery.lightbox.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
+<link href="css/jquery.lightbox.css" rel="stylesheet">
+
+<!--------------END LINKS & SCRIPTS/HEAD--------->  
+  </head>
+<!----------BODY BEGINS HERE---------->
+  <body>
+<!------------NAVIGATION--------------->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+            <a href="#cover"><img src="_images/logosmall.png" style="margin-top: 5px;"></a>
+          <a class="navbar-brand" href="#cover">AMANDA CAVEN</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="#about">About</a></li>
+            <li><a href="#work">Work</a></li>
+            <li><a href="#contact">Contact</a></li>
+            </ul>
+ <!---------------SOCIAL MEDIA ICONS--------------->
+              <ul class="nav navbar-nav navbar-right social">
+                <li><a href="https://www.facebook.com/profile.php?id=1281960096"><i class="fa fa-lg fa-facebook"></i></a></li>
+                <li><a href="http://www.twitter.com/@amanda_caven"><i class="fa fa-lg fa-twitter"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/amandacaven"><i class="fa fa-lg fa-linkedin"></i></a></li>
+                  
+</ul>
+
+        </div>
+      </div>
+    </nav>
+      
+<!----------COVER BEGINS HERE-------------->    
+
+         <section id="cover">
+       <section id="innercover">
+        <img id="logo" src="_images/logo_ACaven.png">
+           <script>
+      
+    
+$("document").ready(function(){
+    $("#logo").animate({top: "50px"}, 950);
+});
+                
+               
+          </script>
+      
+        <span class="tagline">AMANDA CAVEN</span>
+        
+<!--        <hr width="40%">-->
+        
+        <span class="subhead">I'm a graphic & web designer.</span>
+        </section>
+             
+    </section>
+
+ <!--------------------ABOUT SECTION----------------> 
+        <section id="about">
+                      <h1>ABOUT ME</h1>
+            </section>
+    <section class="aboutcontent">
+<!--   <div id="aboutimg"></div>-->
+    <div id="abouttext">My name is Amanda Caven. I love designing for web and print! I've always been fascinated with communication design.  I really enjoy discovering and producing creative solutions, keeping up with the latest design trends, and am always excited to learn new things.</div>
+      </section>
+                  
+
+
+
+<!------------------WORK SECTION----------------->
+      
+
+<!--------LIGHTBOX ATTEMPT---->
+<div class="columns1">
+<div class="column galleries-set1">
+<div id="gallery1" class="gallery">
+	<ul>
+		<li>
+			<a href="_images/mockups/mcleodflyer_mockup.png" title="DESCRIPTION GOES HERE" rel="lightbox[gallery1]">
+				<img src="_images/mockups/mcleodflyer_thumb.png" width="72" height="72" alt="" />
+			</a>
+		</li>
+		<li>
+			<a href="_images/mockups/yakimba_mock.png" title="DESCRIPTION GOES HERE" rel="lightbox[gallery1]" id="anti">
+				<img src="_images/mockups/yakimba_thumb.png" width="72" height="72" alt="" />
+			</a>
+		</li>
+		<li>
+			<a href="_images/mockups/blockprint_mockup.png" title="DESCRIPTION GOES HERE" rel="lightbox[gallery1]">
+				<img src="_images/mockups/blockprint_thumb.png" width="72" height="72" alt="" />
+			</a>
+		</li>
+	</ul>
+    	<ul>
+		<li>
+			<a href="_images/mockups/ctsbanner_mockup.png" title="DESCRIPTION GOES HERE" rel="lightbox[gallery1]">
+				<img src="_images/mockups/ctsbanner_thumb.png" width="72" height="72" alt="" />
+			</a>
+		</li>
+		<li>
+			<a href="_images/mockups/tshirt_mockup.png" title="DESCRIPTION GOES HERE" rel="lightbox[gallery1]">
+				<img src="_images/mockups/tshirt_mockup.jpg" width="72" height="72" alt="" />
+			</a>
+		</li>
+		<li>
+			<a href="photos/image3.jpg" title="DESCRIPTION GOES HERE" rel="lightbox[gallery1]">
+				<img src="_images/mockups/blockprint_mockup.jpg" width="72" height="72" alt="" />
+			</a>	
+		</li>
+	</ul>
+    	<ul>
+		<li>
+			<a href="_images/blockprint_mockup.jpg" title="DESCRIPTION GOES HERE" rel="lightbox[gallery1]">
+				<img src="_images/mockups/blockprint_mockup.jpg" width="72" height="72" alt="" />
+			</a>
+		</li>
+		<li>
+			<a href="_images/banner_mockup.jpg" title="DESCRIPTION GOES HERE" rel="lightbox[gallery1]">
+				<img src="_images/mockups/blockprint_mockup.jpg" width="72" height="72" alt="" />
+			</a>
+		</li>
+		<li>
+			<a href="photos/image3.jpg" title="DESCRIPTION GOES HERE" rel="lightbox[gallery1]">
+				<img src="_images/mockups/blockprint_mockup.jpg" width="72" height="72" alt="" />
+			</a>
+		</li>
+	</ul>
+    </div></div></div><!-- /gallery1 -->
+    
+
+
+ 
+      
+<!--------END LIGHTBOX ATTEMPT FROM LINE25.COM---->
+<!--FIRST GALLERY
+      
+ <div id="work"> 
+     <br>
+    <section id="worksquare">   
+        <div id="square1">
+            <a href="">
+            
+            </a></div>
+        <div id="square2"><a href=""></a></div>
+        <div id="square3"><a href=""></a></div>
+    </section>
+      
+    <section id="worksquare2">   
+        <div id="square4"><a href="">
+            </a></div>
+        <div id="square5"></div>
+        <div id="square6"></div>
+    </section>
+      
+    <section id="worksquare3">   
+        <div id="square7"></div>
+        <div id="square8"></div>
+        <div id="square9"></div>
+    </section>
+     <br>
+      
+      </div>
+      
+--END FIRST GALLERY>
+  
+<!------------CONTACT SECTION---------------->
+  <div id="empty"></div>
+      <section id="container">
+          <h1>GET IN TOUCH</h1>
+      </section>
+    <!----------------Start Contact form ------------------->		                                               <section id="contactarea">
+<div class="formwrap">
+			<div class="containerform">
+				<div class="panel panel-default" style="margin-left: 20.5%; margin-bottom: 0; width:60%; background-color: white;">
+					<div class="panel-heading">
+						<h2 class="panel-title">Send me a message!</h2>
+					</div>
+					<div class="panel-body">
+						<form name="contactform" method="post" action="mailer.php" class="form-horizontal" role="form">
+							<div class="form-group">
+								<label for="inputName" class="col-lg-2 control-label">Name</label>
+								<div class="col-lg-10">
+									<input type="text" class="form-control" id="inputName" name="inputName" placeholder="Your Name">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputEmail1" class="col-lg-2 control-label">Email</label>
+								<div class="col-lg-10">
+									<input type="text" class="form-control" id="inputEmail" name="inputEmail" placeholder="Your Email">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputSubject" class="col-lg-2 control-label">Subject</label>
+								<div class="col-lg-10">
+									<input type="text" class="form-control" id="inputSubject" name="inputSubject" placeholder="Subject Message">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputPassword1" class="col-lg-2 control-label">Message</label>
+								<div class="col-lg-10">
+									<textarea class="form-control" rows="4" id="inputMessage" name="inputMessage" placeholder="Your message to me"></textarea>
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-lg-offset-2 col-lg-10">
+									<button type="submit" class="btn btn-default">
+										Send
+									</button>
+								</div>
+							</div>
+						</form>
+
+					
+				</div>
+			</div>
+		</div>
+    <!---------------End Contact form ------------------>											 
+
+    
+      </section> 
+<!----------------------FOOTER BEGINS HERE------------------->
+        <footer>
+            <br>
+
+Email: contact@amandacaven.com
+            
+
+        </footer>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+ 
+    <script src="bootstrap-3.3.5/dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="bootstrap-3.3.5/docs/assets/js/ie10-viewport-bug-workaround.js"></script>
+    
+    <!------LIGHTBOX SCRIPT---->
+    <script type="text/javascript">
+$(function() {
+	// When DOM is ready, attach the lightBox to the proper elements
+
+	// Left column
+	$('.galleries-set1 .gallery a').lightBox();
+
+//	// Right column
+//	$('#gallery4 a').lightBox();
+//	$('#gallery5 a').lightBox();
+//	$('#gallery6 a').lightBox();
+});
+</script>
+      
+
+  </body>
+</html>
